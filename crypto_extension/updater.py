969960e7d429a9043c143f2457ba7720f1dd7322
SeanEstey <estese@gmail.com>
2018-01-08 18:43:29 -0700

    portfolio table print

--- crypto_extension/updater.py
+++ crypto_extension/updater.py
@@ -2,7 +2,7 @@
 extension directory.
 Docs: https://coinmarketcap.com/api/
 """
-import itertools, requests, json, os, sys, time
+import itertools, requests, json, os, re, sys, time
 from money import Money
 from decimal import Decimal
 from pprint import pprint
@@ -12,31 +12,14 @@ from time import sleep
 WCI_API_KEY = "B8BDV74aQIoF5rQYgZNdQ8VBfdgPN0";
 WCI_MARKETS_URL = "https://www.worldcoinindex.com/apiservice/getmarkets";
 wci_uri = WCI_MARKETS_URL + "?key=" + WCI_API_KEY + "&fiat=cad";
-
-def get_wci_markets():
-
-    r = requests.get(wci_uri)
-    r = json.loads(r.text)
-    pprint(r)
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 EXT_PATH = "%s/Library/Application Support/com.dmitrynikolaev.numi/extensions" % os.path.expanduser("~")
 COINCAP_BASE_URL = "https://api.coinmarketcap.com/v1"
 COINCAP_TICKER_URL = "https://api.coinmarketcap.com/v1/ticker/?convert=CAD&limit=200"
-
 spinner = itertools.cycle(['-', '/', '|', '\\'])
 frequency = 30
-buf = ""
 msg = ''
 currency = "CAD"
+data = []
 coins = [
     {"name":"bitcoin", "symbol":"BTC", "js_name":"BTC_CAD"},
     {"name":"ethereum", "symbol": "ETH", "js_name":"ETH_CAD"},
@@ -63,11 +46,10 @@ coins = [
     {"name":"storj", "symbol": "STORJ", "js_name":"STORJ_CAD"},
     #{"name":"dotcoin", "symbol": "DOT", "js_name":"DOT_CAD"},
 ]
-
 portfolio = [
     {"symbol":"XRB", "amount":6500.0},
     {"symbol":"ETH", "amount":124.0},
-    {"symbol":"IOT", "amount":2685.0},
+    {"symbol":"MIOTA", "amount":2685.0},
     {"symbol":"SC", "amount":124609.0},
     {"symbol":"ADA", "amount":9712.0},
     {"symbol":"SPANK", "amount":13927.0},
@@ -90,6 +72,16 @@ portfolio = [
     {"symbol":"BTC", "amount":0.01}
 ]
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 #----------------------------------------------------------------------
 def update_spinner():
     i=0
@@ -102,38 +94,13 @@ def update_spinner():
         sleep(1)
 
 #----------------------------------------------------------------------
-def get_data():
-
-    global coins
-    buf = ""
-
-    try:
-        r = requests.get(COINCAP_TICKER_URL)
-        data = json.loads(r.text)
-    except Exception as e:
-        print("Request error: %s" % str(e))
-        #pass
-
-    # Get coin prices
-    for coin in coins:
-        for result in data:
-            if result['id'] != coin['name']:
-                continue
-
-            coin['price'] = round(float(result['price_cad']),2)
-            coin['mcap'] = round(float(result['market_cap_cad']),2) + 0.1
-            coin['rank'] = result['rank']
-            coin['percent_change_1h'] = float(result['percent_change_1h'])
-            coin['percent_change_24h'] = float(result['percent_change_24h'])
-            coin['percent_change_7d'] = float(result['percent_change_7d'])
-
-            buf += "%s=%s;  " % (coin['js_name'], coin['price'])
-
+def export_numi(data):
     # Set numi "marketcap" global var
-    r = requests.get(COINCAP_BASE_URL + "/global?convert=%s" % currency)
-    markets = json.loads(r.text)
+    buf = ''
     buf += "\nnumi.setVariable(\"marketcap\", { \"double\":%s, \"unitId\":\"%s\"});" %(
         markets['total_market_cap_cad'], currency)
+
+    buf += "%s=%s;  " % (coin['js_name'], coin['price'])
 
     # Write data to extension
     try:
@@ -148,7 +115,6 @@ def get_data():
 
 #----------------------------------------------------------------------
 def humanize(money):
-
     mag = money.amount.adjusted()
     title = ''
     amount = None
@@ -176,116 +142,121 @@ def colorize(perc):
         bcolors.ENDC)
 
 #----------------------------------------------------------------------
-def print_markets(markets):
+def justify(col, width):
+    rmv_esc = re.compile(r'\x1b[^m]*m')
+    str_val = rmv_esc.sub('',col)
+    escapes = re.findall(rmv_esc, col)
+    widened = str_val.ljust(width)
+    escapes.insert(1, widened)
+    return "".join(escapes)
+
+#----------------------------------------------------------------------
+def get_width(val):
+    rmv_esc = re.compile(r'\x1b[^m]*m')
+    fixed_len = rmv_esc.sub('', str(val))
+    return len(fixed_len)
+
+#----------------------------------------------------------------------
+def get_wci_markets():
+    r = requests.get(wci_uri)
+    r = json.loads(r.text)
+    pprint(r)
+
+#----------------------------------------------------------------------
+def show_markets():
+    r = requests.get(COINCAP_BASE_URL + "/global?convert=%s" % currency)
+    markets = json.loads(r.text)
 
     print("%s\n  Markets\n    Mcap=%s, 24Vol=%s " %(
         datetime.now().strftime("%h %d %H:%M:%S"),
         humanize(Money(markets['total_market_cap_cad']+0.1, 'CAD')),
         humanize(Money(markets['total_24h_volume_cad']+0.1, 'CAD'))))
 
-    print("  Watching (CAD)")
-    sorted_coins = sorted(coins, key=lambda k: k['mcap'])
-
-    header = ["Rank", "Symbol", "Price", "1h", "24h", "7d", "Mcap"]
-    col_widths = [len(col) for col in header]
-    rows = [header]
-
-    for coin in sorted_coins[::-1]:
-        row =[
-            "#%s" % coin['rank'],
-            coin['symbol'],
-            Money(amount=coin['price'], currency='CAD').format('en_US', '$###,###'),
-            str(round(coin["percent_change_1h"],1))+"%",
-            str(round(coin["percent_change_24h"],1))+"%",
-            str(round(coin["percent_change_7d"],1))+"%",
-            humanize(Money(coin['mcap'], 'CAD'))
-        ]
-
-        # TODO: remove esc characters then calculate col_width
-        #import re
-        #re.compile(r'\x1b[^m]*m')
-        for idx in range(0, len(row)):
-            col_widths[idx] = max(len(row[idx]), col_widths[idx])
-
-        rows.append(row)
-
-    for idx in range(0, len(rows)):
-        row = rows[idx]
-
-        if idx == 0:
-            print("    %s%s%s" %(
-                bcolors.BOLD, "".join(row[n].ljust(col_widths[n]+2) for n in range(0,len(row))),bcolors.ENDC))
-            continue
-
-        row[0] = row[0].ljust(col_widths[0]+2)
-        row[1] = row[1].ljust(col_widths[1]+2)
-        row[2] = row[2].ljust(col_widths[2]+2)
-        row[3] =  "%s%s" %(bcolors.FAIL if float(row[3][:-1]) < 0 else bcolors.OKGREEN, row[3].ljust(col_widths[3]+2))
-        row[4] =  "%s%s" %(bcolors.FAIL if float(row[4][:-1]) < 0 else bcolors.OKGREEN, row[4].ljust(col_widths[4]+2))
-        row[5] =  "%s%s" %(bcolors.FAIL if float(row[5][:-1]) < 0 else bcolors.OKGREEN, row[5].ljust(col_widths[5]+2))
-        row[6] = bcolors.ENDC + row[6].ljust(col_widths[6]+2)
-
-        print("    " + "".join(row)) # + bcolors.ENDC)
-
 #----------------------------------------------------------------------
-def print_portfolio():
+def show_watchlist():
+    global coins, data
 
-    import re
-    re.compile(r'\x1b[^m]*m')
+    try:
+        r = requests.get(COINCAP_TICKER_URL)
+        data = json.loads(r.text)
+    except Exception as e:
+        print("Request error: %s" % str(e))
+        #pass
 
-    total = Money(0.0, 'CAD')
-    print("  Portfolio (CAD)")
-
-    header = ['Symbol', 'Amount', 'Price', 'Value', 'Portion']
-    data = [header]
-
-    # Build table data
-    for hold in portfolio:
-        for coin in coins:
-            if coin['symbol'] != hold['symbol']:
+    rows = []
+    for coin in coins:
+        for result in data:
+            if result['id'] != coin['name']:
                 continue
 
-            row =[
-                coin['symbol'],
+            rows.append([
+                result['rank'],
+                result['symbol'],
+                Money(float(result['price_cad']), 'CAD').format('en_US', '$###,###'),
+                colorize(float(result["percent_change_1h"])),
+                colorize(float(result["percent_change_24h"])),
+                colorize(float(result["percent_change_7d"])),
+                humanize(Money(float(result['market_cap_cad']), 'CAD'))
+            ])
+
+    header = ["Rank", "Symbol", "Price", "1h", "24h", "7d", "Mcap"]
+    col_widths = [len(n) for n in header]
+    for row in rows:
+        col_widths = [max(col_widths[n], get_width(row[n])) for n in range(0,len(row))]
+
+    print("  Watching (CAD)")
+    print("    " + "".join(justify(header[n], col_widths[n]+2) for n in range(0,len(header))))
+    for row in sorted(rows, key=lambda x: int(x[0])):
+        print("    " + "".join(justify(row[n], col_widths[n]+2) for n in range(0,len(row))))
+
+#----------------------------------------------------------------------
+def show_portfolio():
+    global coins, portfolio
+    total = 0.0 #Money(0.0, 'CAD')
+    rows = []
+    # Build table data
+    for hold in portfolio:
+        for result in data:
+            if result['symbol'] != hold['symbol']:
+                continue
+
+            total += hold['amount'] * float(result['price_cad'])
+
+            rows.append([
+                result['symbol'],
+                Money(float(result['price_cad']), 'CAD'),
                 hold['amount'],
-                Money(amount=coin['price'], currency='CAD'), #.format('en_US', '$###,###'),
-                Money(round(hold['amount'] * coin['price'],2),'CAD'),
+                Money(round(hold['amount'] * float(result['price_cad']),2),'CAD'),
                 "%##.##"
-            ]
-            total += hold['value']
-            data.append(row)
+            ])
 
-    # Get column widths
-    widths=[0,0,0,0,0]
-    for idx in range(0, len(portfolio)):
-        widths[idx] = max(len(str(portfolio[idx])), widths[idx])
+    rows = sorted(rows, key=lambda x: int(x[3]))[::-1]
+    total = Money(total, 'CAD')
+    header = ['Symbol', 'Price', 'Amount', 'Value', 'Portion']
+    col_widths = [len(n) for n in header]
+    for row in rows:
+        row[4] = str(round((row[3] / total) * 100, 2)) + '%'
+        row[1] = row[1].format('en_US', '$###,###')
+        row[3] = row[3].format('en_US', '$###,###')
+        col_widths = [max(col_widths[n], get_width(row[n])) for n in range(0,len(row))]
 
-    # Print portfolio table
-    for datarow in data:
-        # Portfolio %
-        fmt_row = []
-        datarow[4] = round((datarow[3] / datarow[2])*100,2)
-
-        row = [
-            coin['symbol'],
-            hold['amount'],
-
-        ]
-
-        print("    %s: $%s (%s)" %(
-            hold['symbol'],
-            hold['value'].format('en_US', '###,###', currency_digits=True),
-            str(hold['perc']) + '%'))
-
+    print("\n  Portfolio (CAD)")
+    print("    " + "".join(justify(header[n], col_widths[n]+2) for n in range(0,len(header))))
+    for row in rows: #sorted(rows, key=lambda x: int(x[0])):
+        print("    " + "".join(justify(str(row[n]), col_widths[n]+2) for n in range(0,len(row))))
     print("    ---------------")
-    print("    Total: %s$%s%s" %(
+    print("    %sTotal: %s$%s%s" %(
+        bcolors.BOLD,
         bcolors.OKGREEN,
         total.format('en_US', '###,###', currency_digits=True),
         bcolors.ENDC))
+
 #----------------------------------------------------------------------
 if __name__ == "__main__":
     print("Updating prices in CAD every %ss...\n" % frequency)
+
     while True:
-        get_data()
-        #print_portfolio()
+        #show_markets()
+        show_watchlist()
+        show_portfolio()
         update_spinner()
