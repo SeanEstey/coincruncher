ded47857d36c9534908852115978502884bd9046
SeanEstey <estese@gmail.com>
2018-03-03 22:22:06 -0700

    forgot files

---
+++ docs/config.py
@@ -0,0 +1,24 @@
+# App
+HOST="45.79.176.125"
+MONGO_PORT = 27017
+DB_NAME="crypto"
+DB_DUMP_PATH="~/Dropbox/mongodumps"
+DROPBOXD_PATH="/opt/dropbox/dropboxd"
+
+# Logger
+DEBUGFILE = "logs/debug.log"
+LOGFILE = "logs/info.log"
+MAX_LOG_DATE_WIDTH=14
+MAX_LOG_NAME_WIDTH=8
+MAX_LOG_LINE_WIDTH=100
+LOG_NEWL_INDENT=25
+
+# Client
+DISP_REFRESH_DELAY = 30000
+DISP_SCROLL_SP = 5
+DISP_PAD_HEIGHT = 200
+
+# API
+TICKER_LIMIT=500
+FREQ = 30
+CURRENCY = "cad"
---
+++ docs/data.py
@@ -0,0 +1,85 @@
+# Data to monitor
+
+COINMARKETCAP = {
+    "WATCH": [
+        "LTC",
+        "BCC",
+        "XMR",
+        "NEBL",
+        "BLZ",
+        "ETC"
+    ],
+    "CORR": [
+        "BTC",
+        "BCH",
+        "ETH",
+        "ETC",
+        "XRP",
+        "LTC",
+        "ADA",
+        "XLM",
+        "EOS",
+        "XMR",
+        "NEO",
+        "GAS",
+        "OMG",
+        "NANO",
+        "ICX",
+        "ZCL",
+        "DRGN",
+        "AST",
+        "ODN"
+    ],
+    "PORTFOLIO": {
+        "BNB": 11.17,
+        "BTC": 6.90920,
+        "LINK": 9500,
+        "COV": 640.0,
+        "DBC": 1250.57,
+        "DRGN": 1319.65,
+        "ENJ": 19948.00,
+        "EOS": 247.29,
+        "ETH": 7.08,
+        "GAS": 0.8556,
+        "ICX": 1260.03,
+        "JNT": 749.25,
+        "NANO": 3319.68,
+        "NEO": 100.00,
+        "ODN": 8637.0,
+        "OMG": 77.2,
+        "POLY": 3906.21,
+        "REQ": 2529.44,
+        "SAFEX": 17707.00,
+        "AGI": 4735.75,
+        "VEN": 481.00,
+        "WTC": 100.00,
+        "ZIL": 34721.56,
+        "ZCL": 137
+    }
+}
+
+BINANCE = {
+    "CANDLES": [
+        "BTCUSDT",
+        "ETHUSDT",
+        "BNBUSDT",
+        "LTCUSDT",
+        "BCCUSDT",
+        "ETHBTC",
+        "EOSBTC",
+        "ICNBTC",
+        "LINKBTC",
+        "VENBTC",
+        "NANOBTC",
+        "NANOETH",
+        "NEOBTC",
+        "OMGBTC",
+        "BLZBTC",
+        "ETCBTC",
+        "XMRBTC",
+        "NEBLBTC",
+        "WTCBTC",
+        "NCASHBTC",
+        "ZECBTC"
+    ]
+}
